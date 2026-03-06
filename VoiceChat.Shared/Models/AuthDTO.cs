7686ad4cfa1cdc3360dda2bfe4fb555dfcc3558b
keanu <diede@KEANUDALEX3D>
2026-03-06 23:44:53 +0100

    Ordner strunktur

---
+++ VoiceChat.Shared/Models/AuthDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoiceChat.Shared.Models;
+
+public class LoginRequest
+{
+    public string ClientId { get; set; } = string.Empty;
+    public string? DisplayName { get; set; }
+}
+
+public class LoginResponse
+{
+    public string UserId { get; set; } = string.Empty;
+    public string DisplayName { get; set; } = string.Empty;
+    public string AuthToken { get; set; } = string.Empty;
+}
