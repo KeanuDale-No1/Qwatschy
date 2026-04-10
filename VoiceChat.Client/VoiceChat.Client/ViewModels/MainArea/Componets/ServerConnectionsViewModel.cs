b55db5327f76a7a40d2287007004f17f64955d5a
keanu <diede@KEANUDALEX3D>
2026-04-11 01:00:44 +0200

    Refactoring Client

--- VoiceChat.Client/VoiceChat.Client/ViewModels/MainArea/Componets/ServerConnectionsViewModel.cs
+++ VoiceChat.Client/VoiceChat.Client/ViewModels/MainArea/Componets/ServerConnectionsViewModel.cs
@@ -1,25 +1,44 @@
-﻿using Avalonia.Media.Imaging;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Net;
-using System.Text;
+using System.Linq;
+using Avalonia.Media.Imaging;
 using VoiceChat.Client.ViewModels.Base;
 
 namespace VoiceChat.Client.ViewModels.MainArea.Componets;
 
 
-public record ServerConnectionInfo(string ServerAdress, string ServerName, Bitmap Image);
 
 public partial class ServerConnectionsViewModel : ViewModelBase
 {
-    public ObservableCollection<ServerConnectionInfo> Servers { get; } = new ObservableCollection<ServerConnectionInfo>()
+    public ObservableCollection<ServerConnectionInfo> Servers { get; } = new ObservableCollection<ServerConnectionInfo>();
+
+    public ServerConnectionsViewModel()
     {
-        new ServerConnectionInfo("Test Server 1", "", null),
-        new ServerConnectionInfo("Test Server 2", "", null)
-    }; 
-    
-    
-    
+        
+    }
+
 }
+
+
+public static class AbbreviationHelper
+{
+    public static string GetAbbreviation(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return string.Empty;
+
+        return new string(input
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Where(w => w.Length > 2)
+            .SelectMany(w => w.Where(char.IsUpper))
+            .Take(3)
+            .ToArray());
+    }
+}
+
+public record ServerConnectionInfo(string ServerAdress, string ServerName)
+{
+    public string Abbr => AbbreviationHelper.GetAbbreviation(ServerName);
+}
+
+
