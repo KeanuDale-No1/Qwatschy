2d4a688f5fa8df8062f48af5f5ce406561a5505a
keanu <diede@KEANUDALEX3D>
2026-04-20 22:13:10 +0200

    update

---
+++ VoiceChat.Client/VoiceChat.Client/ViewModels/MainArea/Componets/AddChannelDialogViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoiceChat.Client.ViewModels.MainArea.Componets
+{
+    internal class AddChannelDialogViewModel
+    {
+    }
+}
