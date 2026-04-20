530082bd4cf43714de4ac5b27478104f2acbb3b0
keanu <diede@KEANUDALEX3D>
2026-04-20 22:16:54 +0200

    update

--- VoiceChat.Client/VoiceChat.Client/ViewModels/MainArea/Componets/AddChannelDialogViewModel.cs
+++ VoiceChat.Client/VoiceChat.Client/ViewModels/MainArea/Componets/AddChannelDialogViewModel.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using VoiceChat.Client.Services.DialogService;
+using VoiceChat.Client.ViewModels.Base;
 
-namespace VoiceChat.Client.ViewModels.MainArea.Componets
+namespace VoiceChat.Client.ViewModels.MainArea.Componets;
+
+internal partial class AddChannelDialogViewModel : DialogViewModelBase
 {
-    internal class AddChannelDialogViewModel
+    public override string Title { get; init; } = "Neuen Kanal hinzufügen";
+
+    [ObservableProperty]
+    [NotifyDataErrorInfo]
+    [Required(ErrorMessage = "Ein Name ist erforderlich")]
+    [NotifyCanExecuteChangedFor(nameof(CloseCommand))]
+    private string? channelName;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(CloseCommand))]
+    private string? description;
+
+    public AddChannelDialogViewModel(IDialogService dialogService) : base(dialogService)
     {
     }
+
+    [RelayCommand(CanExecute = nameof(CanClose))]
+    protected async Task Close()
+    {
+        var result = (ChannelName, Description);
+        dialogService.Close(false, result);
+    }
+
+    private bool CanClose() => !HasErrors && !string.IsNullOrWhiteSpace(ChannelName);
 }
